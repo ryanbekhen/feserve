bf408a681700d803ffd1d043e67965bfa4832c63
ACHMAD IRIANTO EKA PUTRA <ryanbekhen.official@gmail.com>
2023-01-19 19:55:08 +0700

    fixed load_location.go doesn't use the timezone parameter

--- internal/logger/load_location.go
+++ internal/logger/load_location.go
@@ -3,7 +3,7 @@ package logger
 import "time"
 
 func loadLocation(timezone string) *time.Location {
-	loc, err := time.LoadLocation("UTC")
+	loc, err := time.LoadLocation(timezone)
 	if err != nil {
 		loc, _ = time.LoadLocation("UTC")
 	}
