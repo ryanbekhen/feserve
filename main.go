d9e01fe5aab91ee297c64632469396b36270da72
ACHMAD IRIANTO EKA PUTRA <ryanbekhen.official@gmail.com>
2023-01-30 18:36:50 +0800

    27 scheduler auto renew (#29)
    * validate ssl using dial connection (#24 )
    * add scheduler for renew ssl

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	mlogger "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/robfig/cron/v3"
 	certificate "github.com/ryanbekhen/feserve/internal/cert"
 	"github.com/ryanbekhen/feserve/internal/config"
 	"github.com/ryanbekhen/feserve/internal/handler"
 	"github.com/ryanbekhen/feserve/internal/logger"
 	mw "github.com/ryanbekhen/feserve/internal/middleware"
 	"github.com/ryanbekhen/feserve/internal/router"
+	"github.com/ryanbekhen/feserve/internal/timeutils"
 )
 
 var conf *config.Config
@@ -32,22 +34,6 @@ func main() {
 	logger := logger.New(logger.Config{
 		Timezone: conf.TimeZone,
 	})
-
-	if conf.Letsencrypt != nil {
-		cert := certificate.NewCert(&certificate.Options{
-			Email:     conf.Letsencrypt.Email,
-			Domains:   conf.Letsencrypt.Domains,
-			CertsPath: conf.Letsencrypt.CertsPath,
-			Debug:     false,
-		})
-
-		if err := cert.ReadFromFile(); err != nil {
-			if err := cert.Generate(); err != nil {
-				logger.Error(err.Error())
-				os.Exit(1)
-			}
-		}
-	}
 
 	app := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
@@ -94,7 +80,56 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 
+	var scheduler *cron.Cron
 	if conf.Letsencrypt != nil {
+		cert := certificate.NewCert(&certificate.Options{
+			Email:     conf.Letsencrypt.Email,
+			Domains:   conf.Letsencrypt.Domains,
+			CertsPath: conf.Letsencrypt.CertsPath,
+			Debug:     false,
+		})
+
+		if err := cert.ReadFromFile(); err != nil {
+			if err := cert.Generate(); err != nil {
+				logger.Error(err.Error())
+				os.Exit(1)
+			}
+		}
+
+		location := timeutils.Location(conf.TimeZone)
+		scheduler := cron.New(cron.WithLocation(location))
+
+		// everyday at 00:00 (0 0 */1 * *)
+		_, err := scheduler.AddFunc("0 0 */1 * *", func() {
+			logger.Info("ssl expiration check")
+			detail, _ := cert.Validate()
+
+			var days float64
+			if detail != nil {
+				days = timeutils.DiffCurtime(detail.Expire, location).Days()
+			}
+
+			// Renew 14 days before expiry
+			if days <= 14 {
+				scheduler.Stop()
+				if err := app.Shutdown(); err != nil {
+					logger.Error(err.Error())
+					os.Exit(1)
+				}
+
+				if err := cert.Generate(); err != nil {
+					logger.Error(err.Error())
+					os.Exit(1)
+				}
+				os.Exit(0)
+			}
+		})
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+		scheduler.Start()
+
 		go func() {
 			certpath := path.Join(conf.Letsencrypt.CertsPath, "ssl.cert")
 			keypath := path.Join(conf.Letsencrypt.CertsPath, "ssl.key")
@@ -142,4 +177,12 @@ func main() {
 		}
 	}()
 	<-stop
+
+	if scheduler != nil {
+		scheduler.Stop()
+	}
+
+	if err := app.Shutdown(); err != nil {
+		logger.Error(err.Error())
+	}
 }
